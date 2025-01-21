2e8d885dda7b72eb24f3c0c6d4780db4b264d130
VictorNaniii <victor.nani@iis.utm.md>
2025-01-21 12:21:37 +0200

    feat: add models for Auth, Product, Review, and TopPage modules; remove obsolete test files

---
+++ src/product/product.model.ts
@@ -0,0 +1,14 @@
+export class PrductModel {
+  image: string;
+  title: string;
+  price: number;
+  oldPrice: number;
+  credit: number;
+  calculatedRating: number;
+  description: string;
+  advantages: string;
+  disAdvantages: string;
+  categories: string[];
+  tags: string[];
+  characteristic: string;
+}
